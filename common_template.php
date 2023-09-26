82ac652a8814df56f82adfedb38da1a436c83f68
Fran <franrocher95@gmail.com>
2023-09-26 20:23:28 +0200

    Updating template

--- common_template.php
+++ common_template.php
@@ -23,27 +23,48 @@
             href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 
-        <nav>
-            <div class="container">
-                <ul>
-                    <li class="home active">
-                        <a href="#whoami">¿Quien soy?</a>
-                    </li>
-                    <li class="about">
-                        <a href="#skills">Habilidades</a>
-                    </li>
-                    <li class="contact">
-                        <a href="#projects">Proyectos</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+ <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">PHP TASKER</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+        <li class="nav-item">
+          <a class="nav-link" href="index.php">Tu perfil</a>
+        </li>
+        <?php
+
+session_start();
+
+
+if (!isset($_SESSION["nombre_usuario"])) {
+    
+        ?>
+        <li class="nav-item">
+          <a class="nav-link" href="login.php">Inicia sesión</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="signin.php" >Registrate</a>
+        </li>
+        <?php
+}
+        ?>
+        <li class="nav-item">
+          <a class="nav-link" href="usersinfo.php" >Mirar tareas de otros usuarios</a>
+        </li>
+      </ul>
+  
+    </div>
+  </div>
+</nav>
 </head>
-<footer class="bg-dark text-center text-white">
+<footer class="bg-dark text-center text-white fixed-bottom">
         <!-- Copyright -->
         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
             © 2023 Email:FranRocher95@gmail.com
         </div>
         <!-- Copyright -->
-    </footer>
+</footer>
 </html>
